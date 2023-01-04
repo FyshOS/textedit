0e758cc1932905824087a1390968f8ad5aaa1fe3
Andy Williams <andy@andy.xyz>
2023-01-04 22:07:59 +0000

    Add more file tests

--- file_test.go
+++ file_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,4 +24,30 @@ func TestLoad(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "Test content", edit.entry.Text)
+}
+
+func TestSave(t *testing.T) {
+	w := test.NewWindow(nil)
+	edit := &textEdit{window: w, changed: binding.NewBool()}
+	ui := edit.makeUI()
+	w.SetContent(ui)
+
+	out, err := storage.Writer(storage.NewFileURI("./testdata/test2.txt"))
+	assert.Nil(t, err)
+	defer os.Remove("./testdata/test2.txt")
+
+	edit.entry.SetText("Testing")
+	err = edit.saveAs(out)
+	assert.Nil(t, err)
+	out.Close()
+
+	data, err := ioutil.ReadFile("./testdata/test2.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, "Testing", string(data))
+
+	edit.entry.SetText("Testing2")
+	edit.save()
+	data, err = ioutil.ReadFile("./testdata/test2.txt")
+	assert.Nil(t, err)
+	assert.Equal(t, "Testing2", string(data))
 }
